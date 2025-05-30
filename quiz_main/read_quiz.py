822af8826005df12a1041743684b0eecd136641f
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-05-30 22:38:40 +0800

    Feat: Add function for read_quiz.py module

--- quiz_main/read_quiz.py
+++ quiz_main/read_quiz.py
@@ -1,1 +1,22 @@
+def read_quiz(file_directory):
+    """
+    Reads the quiz.txt file and extracts each questions into a list
+    Accepts the file directory of the quiz.txt and returns a list with all the questions
+    """
+    
+    # Stores each lines extracted from the quiz.txt
+    lines_list = []  
 
+    # Use a context manager to open and automatically close the file once the lines have been extracted
+    with open(file_directory, "r") as quiz:
+        temp_lines_list = quiz.readlines()  # Stores each lines of the quiz.txt file to a temporary list before stripping      
+        
+        # Loops through the entire list and removes the /n in each line that is in that list
+        for line in temp_lines_list: 
+            line = str(line)  # Converts the lines into a string so that the strip method could be applied
+            if line == "///":  
+                continue
+            else:
+                lines_list.append(line.strip())  
+
+    return lines_list  
