f2fc4a12e3844b453a5e25eb74cb90c1e2e89a5d
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-05-30 22:52:54 +0800

    Feat: Add content for display_question.py module

--- quiz_main/display_question.py
+++ quiz_main/display_question.py
@@ -1,1 +1,13 @@
-
+def display_question(question_dict, question_index):
+    """
+    Displays the item in a questionnaire format.
+    Accepts the dictionary from where the item belongs(question_dict) and the key that corresponds to that item
+    Returns the item in a question-options format
+    """
+    return f"""
+{question_dict[question_index].question}
+a). {question_dict[question_index].option_a}
+b). {question_dict[question_index].option_b}
+c). {question_dict[question_index].option_c}
+d). {question_dict[question_index].option_d}
+"""
