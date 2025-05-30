a3ee8daec683a1a290e64d1df74e6b5f87fec0d8
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-05-30 22:22:22 +0800

    Feat: Add definition for class 'Item'

--- quiz_main/source_quiz.py
+++ quiz_main/source_quiz.py
@@ -1,1 +1,9 @@
+class Item:
 
+    def __init__(self, question, option_a, option_b, option_c, option_d, correct_answer):
+        self.question = question
+        self.option_a = option_a
+        self.option_b = option_b
+        self.option_c = option_c
+        self.option_d = option_d
+        self.correct_answer = correct_answer
