0dc45669f693a83fb0ee89efbbbf76df35dc44fc
Gabriel Paz <gabrieljohnmikhaelh@gmail.com>
2025-05-31 17:18:50 +0800

    Fix: Fix modules missing dependencies
    Fix:
        extract_and_repack:
            add class "Item" definition missing
        shuffle:
            add import random

--- quiz_main/app_modules/extract_and_repack.py
+++ quiz_main/app_modules/extract_and_repack.py
@@ -1,3 +1,13 @@
+class Item:
+
+    def __init__(self, question, option_a, option_b, option_c, option_d, correct_answer):
+        self.question = question
+        self.option_a = option_a
+        self.option_b = option_b
+        self.option_c = option_c
+        self.option_d = option_d
+        self.correct_answer = correct_answer
+
 class ExtractRepack:
     
     def extract_and_repack(lines_list):
--- quiz_main/app_modules/shuffle.py
+++ quiz_main/app_modules/shuffle.py
@@ -1,3 +1,5 @@
+import random
+
 class Shuffle:
 
     def shuffle(min,max):
